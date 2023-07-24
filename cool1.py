e37c033405684c5e897a8528b1c527cdb9e74f1a
evan badding <evanbadding@gmail.com>
2023-07-24 16:34:34 -0600

    cool

--- cool1.py
+++ cool1.py
@@ -9,7 +9,7 @@ options.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,
 # start chrome
 driver = uc.Chrome(options=options)
 
-driver.get('https://messages.google.com')
+driver.get('https://messages.google.com/web/authentication')
 
 # an infinite loop to keep the browser open
 while True:
