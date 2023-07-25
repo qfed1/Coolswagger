462a0e6895edd810d75da34eca71c9e11bc6585e
evan badding <evanbadding@gmail.com>
2023-07-24 18:24:53 -0600

    cool

---
+++ cool2.py
@@ -0,0 +1,48 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotInteractableException
+import time
+
+def start_google_messages(url, phone_number, message_text):
+    options = uc.ChromeOptions()
+    options.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'
+    driver = uc.Chrome(options=options)
+
+    driver.get(url)
+
+    # wait for URL change
+    while True:
+        if driver.current_url == "https://messages.google.com/web/conversations":
+            break
+        time.sleep(5)
+
+    # click start chat button
+    start_chat_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@data-e2e-start-button]")))
+    start_chat_button.click()
+
+    # add number to input field
+    number_input_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@data-e2e-contact-input]")))
+    number_input_field.send_keys(phone_number)
+
+    # press send to button
+    send_to_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-e2e-send-to-button]")))
+    send_to_button.click()
+
+    # input message into textarea
+    message_input_field = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//textarea[@data-e2e-message-input-box]")))
+    message_input_field.send_keys(message_text)
+
+    # send the message
+    try:
+        send_sms_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "floating-button")))
+        send_sms_button.click()
+    except ElementNotInteractableException:
+        print("Button is not interactable at the moment.")
+
+    # Keep the webdriver open
+    while True:
+        time.sleep(5)
+
+start_google_messages('https://messages.google.com/web/authentication', '303 888 3096', 'This works fam')
