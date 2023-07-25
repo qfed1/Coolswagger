1ea3715a1844a88197cdfaf5945dcd52c02c49ac
evan badding <evanbadding@gmail.com>
2023-07-24 19:51:21 -0600

    cool

--- cool2.py
+++ cool2.py
@@ -8,45 +8,30 @@ from gi.repository import Gtk, Gdk
 
 gm = google_messages.GoogleMessages()
 
-class MessageSenderWindow(Gtk.Window):
+class MessageSenderAndCSVReaderWindow(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Google Message Sender")
+        Gtk.Window.__init__(self, title="Google Message Sender and CSV Reader")
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
+        self.layout = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(self.layout)
 
+        # Message sender interface
         self.phone_number_entry = Gtk.Entry()
         self.phone_number_entry.set_text("303 888 3096")
-        vbox.pack_start(self.phone_number_entry, True, True, 0)
+        self.layout.pack_start(self.phone_number_entry, True, True, 0)
 
         self.message_entry = Gtk.Entry()
         self.message_entry.set_text("This works fam")
-        vbox.pack_start(self.message_entry, True, True, 0)
+        self.layout.pack_start(self.message_entry, True, True, 0)
 
-        self.button = Gtk.Button(label="Send Message")
-        self.button.connect("clicked", self.on_button_clicked)
-        vbox.pack_start(self.button, True, True, 0)
+        self.send_button = Gtk.Button(label="Send Message")
+        self.send_button.connect("clicked", self.on_send_button_clicked)
+        self.layout.pack_start(self.send_button, True, True, 0)
 
-    def on_button_clicked(self, widget):
-        phone_number = self.phone_number_entry.get_text()
-        message = self.message_entry.get_text()
-        try:
-            gm.send_message(phone_number, message)
-        except Exception as e:
-            print(f"Exception occurred: {e}")
-            # This delay has been kept here to mimic the original script but may not be necessary.
-            time.sleep(10)
-
-class CSVReaderWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="CSV Reader")
-
-        self.layout = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.add(self.layout)
-
-        self.button = Gtk.Button(label="Open CSV")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.layout.pack_start(self.button, True, True, 0)
+        # CSV reader interface
+        self.open_csv_button = Gtk.Button(label="Open CSV")
+        self.open_csv_button.connect("clicked", self.on_open_csv_button_clicked)
+        self.layout.pack_start(self.open_csv_button, True, True, 0)
 
         self.list_store = Gtk.ListStore(str, str)
         self.tree_view = Gtk.TreeView(self.list_store)
@@ -58,7 +43,17 @@ class CSVReaderWindow(Gtk.Window):
 
         self.layout.pack_start(self.tree_view, True, True, 0)
 
-    def on_button_clicked(self, widget):
+    def on_send_button_clicked(self, widget):
+        phone_number = self.phone_number_entry.get_text()
+        message = self.message_entry.get_text()
+        try:
+            gm.send_message(phone_number, message)
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+            # This delay has been kept here to mimic the original script but may not be necessary.
+            time.sleep(10)
+
+    def on_open_csv_button_clicked(self, widget):
         dialog = Gtk.FileChooserDialog(
             title="Please choose a file", parent=self,
             action=Gtk.FileChooserAction.OPEN,
@@ -80,12 +75,9 @@ class CSVReaderWindow(Gtk.Window):
             for row in reader:
                 self.list_store.append(row[:2])
 
-win = MessageSenderWindow()
+
+win = MessageSenderAndCSVReaderWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
-
-csv_win = CSVReaderWindow()
-csv_win.connect("destroy", Gtk.main_quit)
-csv_win.show_all()
 
 Gtk.main()
