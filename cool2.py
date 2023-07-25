e4c625436704277cf9e1155c1d2fa371747c7fae
evan badding <evanbadding@gmail.com>
2023-07-24 19:08:03 -0600

    cool

--- cool2.py
+++ cool2.py
@@ -1,9 +1,3 @@
-from google_messages import GoogleMessages
+import google_messages
 
-def main():
-    gm = GoogleMessages()
-    gm.authenticate()
-    gm.send_message('303 888 3096', 'This is a test message')
-
-if __name__ == "__main__":
-    main()
+google_messages.gm.send_message('303 888 3096', 'This works fam')
