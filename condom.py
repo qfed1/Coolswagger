b6f1dc4d4a37163606c4b8d9165a201abe39a43d
evan badding <evanbadding@gmail.com>
2023-08-30 11:43:12 -0600

    cool

---
+++ condom.py
@@ -0,0 +1,33 @@
+import undetected_chromedriver.v2 as uc
+import random
+import string
+
+def random_string(length=10):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+# Initialize the options
+options = uc.ChromeOptions()
+
+# Using a random user-agent
+options.add_argument(f"user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{''.join(random.choices(string.digits, k=2))}.0.{''.join(random.choices(string.digits, k=4))}.{''.join(random.choices(string.digits, k=3))} Safari/537.36")
+
+# Random window size
+width = random.randint(800, 1200)
+height = random.randint(600, 900)
+options.add_argument(f"window-size={width},{height}")
+
+# Generate a random profile directory
+random_profile = random_string()
+options.add_argument(f"user-data-dir=./profiles/{random_profile}")
+
+# Initialize Chrome driver with options
+driver = uc.Chrome(options=options)
+
+# Open some website
+driver.get('https://www.google.com')
+
+# Your web automation tasks here
+
+# Finally, close the browser
+driver.quit()
